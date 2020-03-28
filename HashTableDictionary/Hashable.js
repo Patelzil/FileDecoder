549002a3771fa7e191523510e726c8beafdabc02
Patelzil <58107277+Patelzil@users.noreply.github.com>
2020-03-28 02:41:30 -0500

    Abstract methods created

--- HashTableDictionary/Hashable.js
+++ HashTableDictionary/Hashable.js
@@ -2,6 +2,29 @@
 
 class Hashable
 {
+    constructor()
+    {
+        if(this.constructor == Hashable)
+        {
+            throw new Error("don't create an abstract class.");
+        }
+    }// end constructor
+
+    /* hashVal
+     * Purpose - Abstract class needs to be implemented by the subclasses
+     */
+    hashVal()
+    {
+        throw new Error("Missing hashVal in subclass");
+    }// end hashVal
+
+    /* equals
+     * Purpose - Abstract class needs to be implemented by the subclasses
+     */
+    equals(x)
+    {
+        throw new Error("Missing equals function in subclass");
+    }// end equals
 
 }// end class
 
