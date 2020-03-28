56cdae9ad03e6d9a5cf2e9bd2e53e1f8a07a29e7
Patelzil <58107277+Patelzil@users.noreply.github.com>
2020-03-28 02:42:38 -0500

    Implemented methods in IntHash

--- HashTableDictionary/IntHash.js
+++ HashTableDictionary/IntHash.js
@@ -2,7 +2,37 @@
 
 class IntHash extends Hashable
 {
+    constructor(value)
+    {
+        super();
+        this.intVal = value;
+    }// end constructor
 
+    get value()
+    {
+        return this.intVal;
+    }// end value
+
+    /* hashVal
+     * Purpose - returns hash value to be used for insertion
+     */
+    hashVal()
+    {
+        return this.intVal;
+    }// end hashVal
+
+    /* equals
+     * Purpose - checks if the parameter passed is equal to 
+     *          another object of type Hashable
+     * @param x - object used to compare with this object
+     */
+    equals(x)
+    {
+        if(x instanceof IntHash)
+        {
+            return this.value === x.value;
+        }
+    }// end equals
 }// end class
 
 module.exports = IntHash;
--- HashTableDictionary/StringHash.js
+++ HashTableDictionary/StringHash.js
@@ -2,7 +2,37 @@
 
 class StringHash extends Hashable
 {
+    constructor(value)
+    {
+        super();
+        this.strVal = value;
+    }// end constructor
 
+    get value()
+    {
+        return this.strVal;
+    }// end value
+
+    /* hashVal
+     * Purpose - returns hash value to be used for insertion
+     */
+    hashVal()
+    {
+        return this.intVal;
+    }// end hashVal
+
+    /* equals
+     * Purpose - checks if the parameter passed is equal to 
+     *          another object of type Hashable
+     * @param x - object used to compare with this object
+     */
+    equals(x)
+    {
+        if(x instanceof StringHash)
+        {
+            return this.value === x.value;
+        }
+    }// end equals
 }// end class
 
 module.exports = StringHash;
